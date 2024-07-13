b1cca55e1ae842f996893a2f8324b87a96aca63f
Mario Niko <marios-2222@hotmail.com>
2024-07-13 11:20:12 +0300

    created landing page and added local font to the project

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+
+export default function MarketingLayout({
+    children
+}: {
+  children: React.ReactNode
+}
+) {
+  return (
+    <div className="h-full bg-slate-100">
+        {/* {navbar} */}
+        <main className="pt-40 pb-20 bg-slate-100">
+        {children}
+        </main>
+        {/* {footer} */}
+    </div>
+  )
+}
