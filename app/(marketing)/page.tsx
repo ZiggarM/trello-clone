f2d3f9fd8a8a38640f0f4ed780478272c05a4a8e
Mario Niko <marios-2222@hotmail.com>
2024-07-13 11:23:15 +0300

    import Poppins font and implement it on the homepage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import localFont from "next/font/local";
+import { Poppins} from "next/font/google";
 import { Medal } from "lucide-react";
 
 import {cn} from "@/lib/utils";
@@ -9,6 +10,11 @@ import { Button } from "@/components/ui/button";
 
 const headingFont = localFont({
     src: "../../public/fonts/font.woff2",
+})
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 })
 export default function page() {
   return (
@@ -25,7 +31,9 @@ export default function page() {
             <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">work.forward.
             </div>
         </div>
-        <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+        textFont.className
+        )}>
             Collaborate, manage projects, and reach new productivity peaks. From higher rises to the home office, the way your team works is unique - accomplish it all with taskify.
         </div>
         <Button className="mt-6" size="lg" asChild>
