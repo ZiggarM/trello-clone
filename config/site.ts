bac2050db67e9c7f1698ba52e25fced13e8ee6a1
Mario Niko <marios-2222@hotmail.com>
2024-07-13 12:42:52 +0300

    install clerk, create an env and middleware

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+    name: "Taskify",
+    description: "Taskify helps teams move work forward.",
+    url: "https://taskify.vercel.app",
+    ogImage: "https://taskify.vercel.app/og.png",
+    links: {
+        github: "https://github.com/taskify",
+    },
+}
