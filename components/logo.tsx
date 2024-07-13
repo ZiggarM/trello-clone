2744453ad2a69ab8c7fbdf0f53a81a282801b769
Mario Niko <marios-2222@hotmail.com>
2024-07-13 11:32:58 +0300

    Create logo components and add a logo to the project

---
+++ components/logo.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+    src: "../public/fonts/font.woff2",
+})
+
+export default function logo() {
+  return (
+    <Link href="/">
+        <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image 
+        src="/logo.png"
+        alt="logo"
+        width={30}
+        height={30}
+        />
+        <p className={cn("text-lg text-neutral-700 pb-1",
+        headingFont.className
+        )}>
+            Taskify
+        </p>
+        </div>
+    </Link>
+  )
+}
